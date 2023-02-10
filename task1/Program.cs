8377dad61d8eb3dd8b661b84ea7a6a4ffaa10e65
Viktoriya Ivleva <yagodkavi@mail.ru>
2023-02-10 23:15:17 +0500

    добавили папку task1

---
+++ task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число a: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = 0;
+square = number2 * number2;
+if (square == number1) 
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
