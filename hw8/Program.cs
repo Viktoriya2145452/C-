f069674cfc8964e898afd116d02a03ae9c07a362
Viktoriya Ivleva <yagodkavi@mail.ru>
2023-02-12 02:09:57 +0500

    solved home task number 8

---
+++ hw8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+Console.Write("Введите число N: ");
+{int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+if (index == 1)
+{
+    index += 1;
+}
+while (index <= N)
+{
+   Console.WriteLine (index);
+   index += 2;
+}
+}
