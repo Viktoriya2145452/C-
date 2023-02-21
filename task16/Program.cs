b93460a445b6561bf5a4fa57c6396338fc5b86e2
Viktoriya Ivleva <yagodkavi@mail.ru>
2023-02-21 23:44:25 +0500

    added tasks from seminar4

---
+++ task16/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+
+int Pr(int number)
+{
+    int pr = 1;
+    for(int i = 1; i <= number; i++)
+    {
+        pr=pr*i;
+    }
+    return pr;
+}
+
+System.Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine($"Произведение чисел от 1 до N = {Pr(N)}");
---
+++ task17/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+int Num(int n)
+{
+    string ns = n.ToString();
+    return ns.Length;
+}
+
+System.Console.Write("Введите число: ");
+int a = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int result = Num(a);
+System.Console.WriteLine(result);
