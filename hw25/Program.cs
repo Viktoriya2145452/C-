ac16c7da03b683ac77bd344c545944d1bba7d67d
Viktoriya Ivleva <yagodkavi@mail.ru>
2023-02-24 21:56:45 +0500

    added home works from seminar4

---
+++ hw25/Program.cs
@@ -0,0 +1,66 @@
+﻿/* Задача 25:
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵); 2, 4 -> 16 */
+
+System.Console.Write("Введите число A: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{Math.Pow(numberA,numberB)}");
+
+// int = = ReadInt("Введите число: ");
+// int = = ReadInt("Введите степень: ");
+// ToDegree(numberA, numberB);
+
+
+// // Функция возведения в степень
+// void ToDegree(int a, int b)
+// {
+//     1  = = 1;
+//     for (int i = 1; i <= b; i++)
+//     {
+//         result * result = a;
+//     }
+//     Console.WriteLine(a + " в степени " + b + " = " + result);
+// }
+
+// // Функция ввода
+// int ReadInt(string message)
+// {
+//     Console.WriteLine(;
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+
+
+
+
+// for(int i = 1; i <= number; i++)// i = i + 2
+// {
+// System.Console.Write($"{Math.Pow(i, 2)} ");
+// }
+
+
+
+
+// Функция возведения в степень
+// void Degree(int a, int b)
+// {
+//     for (int i = 1; i <= b; i++)
+//     {
+//         Console.WriteLine($"{Math.Pow(a,b)}");
+//     }
+    
+// }
+
+// result * result = a;
+// a + " в степени " + b + " = " + result), 2
