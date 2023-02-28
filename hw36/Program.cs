817ddc12ec5cfddcf8b04735b9f49aedd91edc8c
Viktoriya Ivleva <yagodkavi@mail.ru>
2023-02-28 19:57:44 +0500

    added tasks and home works from seminar5

---
+++ hw36/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArrayWithRandomNumbers(int length)
+{
+int[] array = new int[length];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = rnd.Next(-15,10);
+}
+ return array;
+ }
+
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+int sum = 0;
+
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 1)
+    {
+        sum += array[i];
+    }
+}
+Console.WriteLine($"Сумма элементов нечетных позиций = {sum}");
---
+++ hw38/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] FillArrayWithRandomNumbers(int length)
+{
+int[] array = new int[length];
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+array[i] = rnd.Next(0,100);
+}
+ return array;
+ }
+
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[{string.Join(", ", array)}]");
+double max = array[0];
+double min = array[0];
+for (int i = 0; i < array.Length; i++)
+ {
+     if (array[i] > max)
+     {
+         max = array[i];
+     }
+     if (array[i] < min)
+     {
+         min = array[i];
+     }
+ }
+ Console.WriteLine($"Разница между максимальным и минимальным числом = {max - min}");
